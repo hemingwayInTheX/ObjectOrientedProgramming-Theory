c8e2844622864932ed10cbbc2009747987d8b7d2
hemingwayInTheX <leeht620@gmail.com>
2021-04-17 23:45:37 +0900

    try to study the order of <Constructor> calls through the classInheritance relationship

--- Preview-CoreThory_OOP/CH_Preview_Inheritance.cpp
+++ Preview-CoreThory_OOP/CH_Preview_Inheritance.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-
+/*재귀함수의 호출 순서 뉘앙스를 생각해보자.*/
+/*생성자의 호출 순서는 부모 클래스 부터 자식 클래스 순으로!*/
 class TV {
 public:
 	TV() { size = 20; }
-	TV(int size) { this->size = size; }
+	TV(int size) { this->size = size; }/*1. 상위 클래스 생성자 부터 호출*/
 	int getSize() { return size; }
 private:
 	int size;
@@ -12,7 +13,7 @@ private:
 
 class WideTV : public TV {
 public:
-	WideTV(int size, bool videoln) :TV(size) {this->videoln = videoln;}
+	WideTV(int size, bool videoln) :TV(size) {this->videoln = videoln;}/*2. 그 다음 호출*/
 	bool getVideoln() { return videoln; }
 private:
 	bool videoln;
@@ -20,7 +21,7 @@ private:
 
 class SmartTV :public WideTV {
 public:
-	SmartTV(string ipAdrr, int size) :WideTV(size, true) {this->ipAddr = ipAddr;}
+	SmartTV(string ipAdrr, int size) :WideTV(size, true) {this->ipAddr = ipAddr;}/*마지막 호출*/
 	string getIpAddr() { return ipAddr; }
 private:
 	string ipAddr;
